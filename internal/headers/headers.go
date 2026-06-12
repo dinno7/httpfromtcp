0fb4d8bacd405f17d980ea53680d35b79cd81041
dinno <tahadlrb7@gmail.com>
2026-06-12 23:16:56 +0330

    feat: add header parsing

---
+++ internal/headers/headers.go
@@ -0,0 +1,99 @@
+package headers
+
+import (
+	"bytes"
+	"errors"
+	"strings"
+)
+
+const CRLF = "\r\n"
+
+var (
+	ErrInvalidHeaderLine               = errors.New("invalid header line")
+	ErrInvalidWhitespaceAfterHeaderKey = errors.New("whitespace after header key is not valid")
+	ErrInvalidCharacter                = errors.New("invalid charactor")
+)
+
+type Headers map[string]string
+
+func NewHeaders() Headers {
+	return make(Headers)
+}
+
+func (h Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+	for {
+		index := bytes.Index(data[read:], []byte(CRLF))
+		if index == -1 {
+			break
+		}
+
+		if index == 0 {
+			done = true
+			read += len(CRLF)
+			break
+		}
+
+		key, value, err := parseHeaderLine(data[read : read+index])
+		if err != nil {
+			return 0, false, err
+		}
+
+		h.Set(key, value)
+
+		read += index + len(CRLF)
+	}
+
+	return read, done, nil
+}
+
+func (h Headers) Get(key string) string {
+	key = strings.ToLower(key)
+	return h[key]
+}
+
+func (h Headers) Set(key, value string) {
+	key = strings.ToLower(key)
+	h[key] = value
+}
+
+func parseHeaderLine(data []byte) (string, string, error) {
+	parts := bytes.SplitN(data, []byte{':'}, 2)
+	if len(parts) != 2 {
+		return "", "", ErrInvalidHeaderLine
+	}
+	key := bytes.TrimPrefix(parts[0], []byte{' '})
+	if bytes.HasSuffix(key, []byte{' '}) {
+		return "", "", ErrInvalidWhitespaceAfterHeaderKey
+	}
+
+	if !isValidHeaderKey(key) {
+		return "", "", ErrInvalidCharacter
+	}
+
+	value := bytes.TrimSpace(parts[1])
+	return string(key), string(value), nil
+}
+
+func isValidHeaderKey(key []byte) bool {
+	// Valid characters:
+	// Uppercase letters: A-Z
+	// Lowercase letters: a-z
+	// Digits: 0-9
+	// Special characters: !, #, $, %, &, ', *, +, -, ., ^, _, `, |, ~
+	for i := range key {
+		c := key[i]
+		isValid := (c >= 'A' && c <= 'Z') ||
+			(c >= 'a' && c <= 'z') ||
+			(c >= '0' && c <= '9') ||
+			(c == '!' || c == '#' || c == '$' || c == '%' || c == '&' || c == '\'' ||
+				c == '*' || c == '+' || c == '-' || c == '.' || c == '^' || c == '_' ||
+				c == '`' || c == '|' || c == '~')
+
+		if !isValid {
+			return false
+		}
+	}
+	return true
+}
---
+++ internal/headers/headers_test.go
@@ -0,0 +1,37 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeaders(t *testing.T) {
+	// Test: Valid single header
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\nFoo: Bar\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers.Get("Host"))
+	assert.Equal(t, "", headers["NotExists"])
+	assert.Equal(t, 35, n)
+	assert.True(t, done)
+
+	// Test: Invalid spacing header
+	headers = NewHeaders()
+	data = []byte("       Host : localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.ErrorIs(t, err, ErrInvalidWhitespaceAfterHeaderKey)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Invalid character:
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.ErrorIs(t, err, ErrInvalidCharacter)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
