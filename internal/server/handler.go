e045488470c548447313b036cbde2e915f343647
dinno <tahadlrb7@gmail.com>
2026-06-14 02:12:00 +0330

    refactor(handler): get response instead of io.Writer

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -2,29 +2,23 @@ package server
 
 import (
 	"fmt"
-	"io"
-	"net"
 
 	"github.com/dinno7/httpfromtcp/internal/request"
 	"github.com/dinno7/httpfromtcp/internal/response"
 )
 
-type Handler func(w io.Writer, req *request.Request) *HandlerError
+type Handler func(w *response.Response, req *request.Request) *HandlerError
 
 type HandlerError struct {
 	StatusCode response.StatusCode
 	Message    []byte
 }
 
-func (he *HandlerError) Write(conn net.Conn) error {
-	if err := response.WriteStatusLine(conn, he.StatusCode); err != nil {
+func (he *HandlerError) Write(r *response.Response) error {
+	if err := r.WriteStatusLine(he.StatusCode); err != nil {
 		return err
 	}
-	headers := response.GetDefaultHeaders(len(he.Message))
-	if err := response.WriteHeaders(conn, headers); err != nil {
-		return err
-	}
-	_, err := conn.Write(he.Message)
+	_, err := r.Write(he.Message)
 	return err
 }
 
