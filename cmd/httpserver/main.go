eaf4413ea9bc0cc02d002d29608b219c6b7daa20
dinno <tahadlrb7@gmail.com>
2026-06-14 20:42:54 +0330

    refactor(cmd/httpserver): add some stuff to pkg client

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/dinno7/httpfromtcp/internal/request"
@@ -16,19 +21,47 @@ const port = 42069
 
 func main() {
 	handler := func(w *response.Response, req *request.Request) *server.HandlerError {
-		switch req.RequestLine.Method {
-		case "GET":
-			switch req.RequestLine.RequestTarget {
-			case "/":
-				w.Write([]byte("{\"age\": 25}"))
-			case "/error":
+		if req.RequestLine.Method == "GET" {
+			if req.RequestLine.RequestTarget == "/" {
+				w.Headers().Set("Content-Type", "text/html")
+				w.Write(
+					[]byte(
+						"<html> <head> <title>200 OK</title> </head> <body> <h1>Success!</h1> <p>Your request was an absolute banger.</p> </body> </html>",
+					),
+				)
+
+			} else if req.RequestLine.RequestTarget == "/error" {
 				msg, _ := json.Marshal(map[string]any{
 					"ok":      false,
 					"message": "God Damn",
 				})
+				w.Headers().Set("Content-Type", "application/json")
 				return server.NewHandlerErrorBadRequest(msg)
-			default:
-				return server.NewHandlerErrorNotFound([]byte("Not found!"))
+			} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbun") {
+				targetUrl := strings.TrimPrefix(req.RequestLine.RequestTarget, "/httpbun/")
+				buf := make([]byte, 1024)
+				for {
+					result, err := http.Get(fmt.Sprintf("https://httpbun.com/%s", targetUrl))
+					if err != nil {
+						return server.NewHandlerErrorInternalServerError([]byte(err.Error()))
+					}
+					reader := result.Body
+					n, err := reader.Read(buf)
+					if err != nil {
+						if errors.Is(err, io.EOF) {
+							break
+						}
+						return server.NewHandlerErrorInternalServerError([]byte(err.Error()))
+					}
+					w.WriteChunkedBody(buf[:n])
+				}
+				w.WriteChunkedBodyDone()
+			} else {
+				return server.NewHandlerErrorNotFound(
+					[]byte(
+						"<html> <head> <title>404 Not Found</title> </head> <body> <h1>Not Found!</h1> <p>No anything found for your path.</p> </body> </html>",
+					),
+				)
 			}
 		}
 
